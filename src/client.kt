c822103cf6e791759d8a016b31ffad955da65edf
Maluki Muthusi <malukimuthusi@yahoo.com>
2020-02-20 17:01:06 +0300

    Initialized Project

---
+++ src/client.kt
@@ -0,0 +1,46 @@
+import java.io.BufferedReader
+import java.io.DataInputStream
+import java.io.DataOutputStream
+import java.io.InputStreamReader
+import java.net.Socket
+
+object TCPClient {
+    @Throws(Exception::class)
+    @JvmStatic
+    fun main(args: Array<String>) {
+        try {
+            // host and port number.
+            val socket = Socket("127.0.0.1", 8888)
+
+            // read from the socket.
+            val inStream = DataInputStream(socket.getInputStream())
+
+            // Write to the socket.
+            val outStream = DataOutputStream(socket.getOutputStream())
+
+            // Read from the keyboard.
+//            val br = BufferedReader(InputStreamReader(System.`in`))
+
+            var clientMessage = ""
+            var serverMessage = ""
+
+            while (clientMessage != "bye") {
+                println("Type message:")
+                clientMessage = readLine()!!
+
+                // Write to the socket.
+                outStream.writeUTF(clientMessage)
+                outStream.flush()
+
+                // read from socket.
+                serverMessage = inStream.readUTF()
+                println("From Server: $serverMessage")
+            }
+            outStream.close()
+            outStream.close()
+            socket.close()
+        } catch (e: Exception) {
+            println(e)
+        }
+    }
+}
---
+++ src/server.kt
@@ -0,0 +1,50 @@
+import java.io.BufferedReader
+import java.io.DataInputStream
+import java.io.DataOutputStream
+import java.io.InputStreamReader
+import java.net.ServerSocket
+
+object TCPServer {
+    @Throws(Exception::class)
+    @JvmStatic
+    fun main(args: Array<String>) {
+        try {
+            // create Server socket bound to port 8888.
+            val server = ServerSocket(8888)
+
+            // Listen for connection and accept. Returns a socket object.
+            val serverClient = server.accept()
+
+            // bind the server to an ip address.
+//            server.bind(192.168.19.2)
+
+            val inStream = DataInputStream(serverClient.getInputStream())
+            val outStream = DataOutputStream(serverClient.getOutputStream())
+
+//            val reader = BufferedReader(InputStreamReader(System.`in`))
+            var clientMessage = ""
+            var serverMessage: String? = ""
+
+            while (clientMessage != "bye") {
+                clientMessage = inStream.readUTF()
+                println("From Client: $clientMessage")
+                println("Type your message:")
+                serverMessage = readLine()!!
+                outStream.writeUTF(serverMessage.toString())
+                outStream.flush()
+            }
+
+            inStream.close()
+            outStream.close()
+
+            // close connection with this client.
+            serverClient.close()
+
+            // close this socket.
+            server.close()
+
+        } catch (e: Exception) {
+            println(e)
+        }
+    }
+}
